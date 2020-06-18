fc92cb355bd08cc9beee20dee98b488755fdfa1d
David Rios <davidsamuelrios02@gmail.com>
2020-06-18 10:50:21 -0400

    Starting Tecnonautas AppBar

---
+++ lib/src/widgets/tecnonautas_appbar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TecnonautasAppbar extends StatelessWidget with PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => throw UnimplementedError();
+}
